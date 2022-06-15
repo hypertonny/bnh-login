43a074d80fd4b6a39f2803bc91f4f04ad2dd4abc
BholeNath Hacker YT <57041353+hypertonny@users.noreply.github.com>
2022-06-16 00:23:18 +0530

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,38 @@
+
+#!/bin/bash 
+clear
+echo
+pkg install pv -y >/dev/null 2>&1
+echo -e "\033[32m\033[1m]────────────────────────────────────────────["
+echo -e "\033[33m\033[1m   Installing Packages and All Dependencies" | pv -qL 10
+apt update                    
+apt upgrade -y 
+pkg install python -y 
+pkg install cmatrix -y 
+pkg install pv -y 
+apt install figlet -y  
+apt install ruby -y 
+apt install mpv -y 
+pip install lolcat 
+pip install random 
+pip install requests 
+pkg install python2 -y 
+pkg install termux-api -y 
+echo -e "\033[31m\033[1m        INSTALLED SUCCESSFULLY \033[32m[\033[36m✓\033[32m]" | pv -qL 12
+echo -e "\033[33m\033[1m]────────────────────────────────────────────["
+termux-setup-storage
+cd $HOME 
+cd bnh-login 
+cp login.sh $PREFIX/etc
+cd $HOME
+cd bnh-login 
+chmod +x login.sh delete.sh setup.sh sound_effect.py banner.sh 
+mkdir Song 
+mv Access-Granted.mp3 wait2.mp3 wait.mp3 sound_effect.py Song
+rm 1
+mv network.py /sdcard
+
+cd $HOME 
+cd bnh-login 
+
+bash login.sh
